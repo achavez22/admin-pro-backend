44648b7154c3d72ddcaa4e0056c6a86c0cdd45d3
Adan Chavez OLivera <adanch22@gmail.com>
2023-12-08 16:00:27 -0600

    add spa control

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const  cors = require('cors');
 const {dbConnection} = require('./database/config'); 
-
+const path = require('path');
 //Crear el servidor de express 
 const app = express();
 
@@ -25,6 +25,11 @@ app.use( '/api/hospitales', require('./routes/hospitales') );
 app.use( '/api/medicos', require('./routes/medicos') );
 app.use( '/api/todo', require('./routes/busquedas') );
 app.use( '/api/upload', require('./routes/uploads') );
+
+//Lo ultimo
+app.get('*', (req, resp) => {
+    resp.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
 
 app.listen(process.env.PORT, ()=>{
     console.log('Run server in port:' + process.env.PORT);
